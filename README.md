4fd492d9abf50d16cb98878c91b6070cf4b721e4
AxeemHaider <axeemhaider@gmail.com>
2024-07-24 10:51:58 +0500

    adding google font Inter

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # inbzar
+
+## Start
+
+First install npm packages
+
+- `npm install`
+
+Second run hugo server for dev
+
+- `hugo server`
+
+For build run just `hugo`
--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,13 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 const mainDir = __dirname + "/../../";
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     `${mainDir}/layouts/**/*.html`,
     `${mainDir}/content/**/*.md`,
     `${mainDir}/content/**/*.html`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {},
   plugins: [],
