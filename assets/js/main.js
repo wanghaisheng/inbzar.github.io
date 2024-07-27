9dfe81aefea70a621709d7b795ba3b35acf8b526
AxeemHaider <axeemhaider@gmail.com>
2024-07-27 14:40:34 +0500

    adding referral code and submitted data to google forms

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,15 @@ const toggleMenu = () => {
   document.getElementById("mobile-menu").classList.toggle("hidden");
   document.getElementById("header").classList.toggle("isolate");
 };
+
+function getQueryParam(name) {
+  const params = new URLSearchParams(window.location.search);
+  return params.get(name);
+}
+
+window.onload = function () {
+  const referralCode = getQueryParam("ref");
+  if (referralCode) {
+    localStorage.setItem("referral-code", referralCode);
+  }
+};
